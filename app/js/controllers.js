eeab179b3239ae9e281e730784867381eccf625a
Jonas Pollok <jonas.p@gmail.com>
2013-08-14 16:26:13 +0200

    export feature

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,7 +3,25 @@
 /* Controllers */
 
 angular.module('mathador.controllers', []).
-	controller('MathadorCtrl',['$scope', 'editor', function($scope, editor) {
+	controller('MathadorCtrl',['$scope', 'editor', '$location', function($scope, editor, $location) {
+		console.log($location);
+		console.log($location.path());
+		// $location.url() - $location.path()
+		var paramsString = $location.url().split($location.path())[1];
+
+		var params = {}
+		if (paramsString.length > 0 && paramsString.indexOf('?') == 0) {
+			paramsString = paramsString.slice(1);
+			var paramsList = paramsString.split('&');
+			for (var i = 0; i < paramsList.length; i++) {
+				var split = paramsList[i].split('=');
+				if (split.length == 2) {
+					params[split[0]] = split[1];
+				}
+			}
+		}
+
+		$scope.export = params.export;
 
 		$scope.$watch( function () { return editor.lines }, function (data) {
 			$scope.lines = data;
