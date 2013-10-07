73a85e8249933dacb07b9ba7006e686323227444
Jonas Pollok <jonas.p@gmail.com>
2013-10-07 17:52:50 +0200

    view only, no edit mode

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,8 +3,21 @@
 /* Controllers */
 
 angular.module('mathador.controllers', []).
-	controller('MathadorCtrl',[ '$scope', '$location', 'docId',
-		function($scope, $location, docId) {
+	controller('ViewCtrl', ['$scope', 'docId',
+		function($scope, docId) {
+			sharejs.open(docId, 'text', function (error, doc) {
+				$scope.$apply(function () {
+					$scope.content = doc.snapshot;
+				});
+				doc.on('change', function(op) {
+					$scope.$apply(function () {
+						$scope.content = doc.snapshot;
+					});
+				});
+			});
+		}]).
+	controller('MathadorCtrl',['$scope', 'docId',
+		function($scope, docId) {
 			$scope.aceLoaded = function (editor) {
 				sharejs.open(docId, 'text', function (error, doc) {
 					doc.attach_ace(editor);
