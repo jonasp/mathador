4c188af595dc33cfb6a977c71dab4dbff9ea6e25
Jonas Pollok <jonas.p@gmail.com>
2013-10-07 17:48:12 +0200

    remove unneeded export functionality

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -10,22 +10,5 @@ angular.module('mathador.controllers', []).
 					doc.attach_ace(editor);
 				});
 			};
-
-			// $location.url() - $location.path()
-			var paramsString = $location.url().split($location.path())[1];
-
-			var params = {}
-			if (paramsString && paramsString.length > 0 && paramsString.indexOf('?') == 0) {
-				paramsString = paramsString.slice(1);
-				var paramsList = paramsString.split('&');
-				for (var i = 0; i < paramsList.length; i++) {
-					var split = paramsList[i].split('=');
-					if (split.length == 2) {
-						params[split[0]] = split[1];
-					}
-				}
-			}
-
-			$scope.export = params.export;
 		}
 	]);
