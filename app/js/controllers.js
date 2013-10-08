bbb2a15c318f2d23b9cbb5368336b314ee2a5119
Jonas Pollok <jonas.p@gmail.com>
2013-10-08 12:43:32 +0200

    view modes

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,25 +3,7 @@
 /* Controllers */
 
 angular.module('mathador.controllers', []).
-	controller('ViewCtrl', ['$scope', 'docId',
-		function($scope, docId) {
-			sharejs.open(docId, 'text', function (error, doc) {
-				$scope.$apply(function () {
-					$scope.content = doc.snapshot;
-				});
-				doc.on('change', function(op) {
-					$scope.$apply(function () {
-						$scope.content = doc.snapshot;
-					});
-				});
-			});
-		}]).
-	controller('MathadorCtrl',['$scope', 'docId',
-		function($scope, docId) {
-			$scope.aceLoaded = function (editor) {
-				sharejs.open(docId, 'text', function (error, doc) {
-					doc.attach_ace(editor);
-				});
-			};
-		}
-	]);
+	controller('navCtrl', ['$scope', '$state', '$stateParams', function ($scope, $state, $stateParams) {
+		$scope.state = $state;
+		$scope.params= $stateParams;
+	}]);
